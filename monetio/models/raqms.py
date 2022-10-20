1b7d42463d36cb67afc7d1c1d6a04ffe55005ed6
Maggie Bruckner <mbruckner@raqms-ops.ssec.wisc.edu>
2022-10-20 14:01:21 +0000

    merge fixes

--- monetio/models/raqms.py
+++ monetio/models/raqms.py
@@ -1,106 +1,151 @@
-# Reader for RAQMS real-time files.
+"""
+Reader for RAQMS real-time files.
 
+RAQMS: Realtime Air Quality Monitoring System
+
+More information: http://raqms-ops.ssec.wisc.edu/
+"""
 import xarray as xr
 
+
 def open_dataset(fname):
-    '''Open a single dataset from RAQMS output. Currently set for netcdf file format.
+    """Open a single dataset from RAQMS output. Currently expects netCDF file format.
+
     Parameters
     ----------
-    fname : string
-        Filename to be opened.
+    fname : str
+        File to be opened.
+
     Returns
     -------
     xarray.Dataset
-    '''
+    """
     names, netcdf = _ensure_mfdataset_filenames(fname)
-    try:
-        if netcdf:
-            f = xr.open_dataset(names[0],drop_variables=['theta'])
-            f = _fix_grid(f)
-            f = _fix_time(f)
-        elif 'trimmed' in fname[0]:
-            #print('opening')
-            f = xr.open_dataset(fname[0])
-        else:
-            raise ValueError
-    except ValueError:
-        print('''File format not recognized. Note that files should be
-                preprocessed to netcdf.''')
-    #if 'latitude' not in f.coords: print('coordinate issue')
-    return f
+    if not netcdf:
+        raise ValueError(
+            "File format not supported. Note that files should be preprocessed to netCDF."
+        )
+
+    ds = xr.open_dataset(names[0], drop_variables=["theta"])
+    ds = _fix_grid(ds)
+    ds = _fix_time(ds)
+
+    return ds
+
 
 def open_mfdataset(fname):
-    '''Open a multiple file dataset from RAQMS output.
+    """Open a multiple file dataset from RAQMS output.
+
     Parameters
     ----------
-    fname : string
-        Filenames to be opened
+    fname : str or list of str
+        Files to be opened, expressed as a glob string or list of string paths.
+
     Returns
     -------
     xarray.Dataset
-    '''
+    """
     names, netcdf = _ensure_mfdataset_filenames(fname)
-    try:
-        if netcdf:
-            #print('passed file check and now reading')
-            f = xr.open_mfdataset(names, concat_dim='time',drop_variables=['theta'],combine='nested')
-            f = _fix_grid(f)
-            f = _fix_time(f)
-        elif 'trimmed' in fname[0]:
-            f = xr.open_dataset(fname)
-        else:
-            raise ValueError
-    except ValueError:
-        print('''File format not recognized. Note that files should be in netcdf
-                format. Do not mix and match file types.''')
+    if not netcdf:
+        raise ValueError(
+            "File format not supported. Note that files should be "
+            "in netCDF format."
+            "Do not mix and match file types."
+        )
 
-    return f
+    ds = xr.open_mfdataset(names, concat_dim="time", drop_variables=["theta"], combine="nested")
+    ds = _fix_grid(ds)
+    ds = _fix_time(ds)
 
-def _fix_grid(f):
-    from  numpy import meshgrid
-    
-    lat = f.lat.values
-    lon = f.lon.values
-    lon[(lon > 180)] -= 360
-    lon,lat = meshgrid(lon,lat)
-    f = f.rename({'lat':'y','lon':'x','lev':'z'})
-    f['longitude']=(('y','x'),lon)
-    f['latitude']=(('y','x'),lat)
-    f = f.set_coords(['latitude','longitude'])
-    del lon,lat
-    return f
+    return ds
 
-def _fix_time(f):
+
+def _fix_grid(ds):
+    from numpy import meshgrid
+
+    # Create 2-D lat/lon grid with dims ('y', 'x') and lon in [-180, 180)
+    lat = ds.lat.values
+    lon = ds.lon.values
+    lon[(lon >= 180)] -= 360
+    lon, lat = meshgrid(lon, lat)
+    ds = ds.rename_dims({"lat": "y", "lon": "x", "lev": "z"}).drop_vars(["lat", "lon"])
+    ds["longitude"] = (
+        ("y", "x"),
+        lon,
+        {
+            "long_name": "Longitude",
+            "units": "degree_east",
+            "standard_name": "longitude",
+        },
+    )
+    ds["latitude"] = (
+        ("y", "x"),
+        lat,
+        {
+            "long_name": "Latitude",
+            "units": "degree_north",
+            "standard_name": "latitude",
+        },
+    )
+    ds = ds.reset_coords().set_coords(["latitude", "longitude"])
+    del lon, lat
+
+    # Add attrs for 'lev'
+    # The 'lev' values are nominal and should be the same among files
+    ds["lev"].attrs.update(
+        long_name="Nominal potential temperature of model level",
+        units="K",
+        description=(
+            "In the stratosphere (beginning at lev=492), the model levels are on potential temperature surfaces. "
+            "Below lev=492, the model levels are a blend of potential temperature and sigma (terrain-following) coordinates."
+        ),
+    )
+
+    # Invert in z so that index 0 is closest to surface
+    # https://github.com/pydata/xarray/discussions/6695
+    ds = ds.isel(z=slice(None, None, -1))
+
+    return ds
+
+
+def _fix_time(ds):
+    """Set 'time' coordinate variable based on the date/time strings."""
     import pandas as pd
-    dtstr = f.Times.values
-    dtstr = [i.decode() for i in dtstr]
-    date = pd.to_datetime(dtstr,format='%Y_%m_%d_%H:%M:%S')
-    f['Time'] = (('time',),date)
-    
-    f = f.set_index({'time':'Time'})
-    del date,dtstr
-    return f
+
+    dtstr = ds.Times.values.astype(str)
+
+    time = pd.to_datetime(dtstr, format=r"%Y_%m_%d_%H:%M:%S")
+    ds["time"] = (("time",), time)
+
+    # These time variables are no longer needed
+    ds = ds.drop_vars(["IDATE", "Times"], errors="ignore")
+
+    return ds
+
 
 def _ensure_mfdataset_filenames(fname):
-    '''Checks if RAQMS netcdf dataset
+    """Checks if RAQMS netcdf dataset
+
     Parameters
     ----------
-    fname : string or list of strings
+    fname : str or list of str
+
     Returns
     -------
-    type
-    '''
+    list of str
+        The file paths.
+    bool
+        Whether all of files are the expected uwhyb netCDF format.
+    """
     from glob import glob
-    from numpy import sort
-    import six
+    from os.path import basename
 
-    if isinstance(fname, six.string_types):
-        names = sort(glob(fname))
+    if isinstance(fname, str):
+        fpaths = sorted(glob(fname))
     else:
-        names = sort(fname)
-    netcdfs = [True for i in names if 'nc' in i if 'uwhyb' in i]
-    netcdf = False
-    if len(netcdfs) >= 1:
-        netcdf = True
-    print(names)
-    return names, netcdf
+        fpaths = sorted(fname)
+
+    # Check file name is of the expected format
+    good = len(fpaths) > 0 and all(fp.endswith(".nc") and "uwhyb" in basename(fp) for fp in fpaths)
+
+    return fpaths, good
