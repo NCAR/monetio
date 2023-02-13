23ee5c5477efc0b4db8e89e95f398dc05d462082
mebruckner <48494069+mebruckner@users.noreply.github.com>
2023-02-13 11:04:59 -0600

    Update raqms.py
    remove double of _fix_pres

--- monetio/models/raqms.py
+++ monetio/models/raqms.py
@@ -133,23 +133,6 @@ def _fix_time(ds):
 
     return ds
 
-def _fix_pres(ds):
-    """Rename pressure variables and convert from mb to Pa."""
-    rename0 = {
-        "psfc": "surfpres_pa",
-        "delp": "dp_pa",
-        "pdash": "pres_pa_mid",
-    }
-    rename = {k: v for k, v in rename0.items() if k in ds.variables}
-
-    ds = ds.rename_vars(rename)
-    for vn in rename.values():
-        assert ds[vn].attrs.get("units", "mb") in {"mb", "hPa"}
-        with xr.set_options(keep_attrs=True):
-            ds[vn] *= 100
-        ds[vn].attrs.update(units="Pa")
-
-    return ds
 
 def _fix_pres(ds):
     """Rename pressure variables and convert from mb to Pa."""
