62a40d7f53614665f844e798b11ef59f4404a70e
mebruckner <48494069+mebruckner@users.noreply.github.com>
2024-02-26 10:12:05 -0600

    correct merge conflicts

--- monetio/sat/_omps_l3_mm.py
+++ monetio/sat/_omps_l3_mm.py
@@ -1,68 +1,83 @@
-def read_OMPS_l3(files):
-    """Loop to open OMPS nadir mapper Total Column Ozone L3 files.
-    Parameters:
-    -----------
-    files: string or list of strings
+"""Read NASA Suomi NPP OMPS Level 3 Nadir Mapper TO3 file."""
+from pathlib import Path
 
-    returns xarray Dataset.
-    """
-    
-    from glob import glob
-    import xarray as xr
-    import numpy as np
-    from matplotlib import pyplot as plt
-    
-    start_dataset = True
-    times = []
-    filelist = sorted(glob(files,recursive=False))
-    
-    for filename in filelist:
-        data = extract_OMPS_l3(filename)
-        times.append(data.attrs['time'])
-        del data.attrs['time']
-        if start_dataset:
-            data_array = data
-            start_dataset = False
-        else:
-            data_array = xr.concat([data_array,data],'time')
-    data_array['time'] = (('time'),np.array(times))
-    data_array = data_array.reset_coords().set_coords(['latitude','longitude','time'])
-    return data_array
 
-def extract_OMPS_l3(fname):
-    '''Read locally stored NASA Suomi NPP OMPS Level 3 Nadir Mapper TO3 files
+def open_dataset(files):
+    """Open OMPS nadir mapper Total Column Ozone L3 files.
+
     Parameters
     ----------
-    fname: string
-        fname is local path to h5 file
+    files: str or Path or list
+        Input file path(s).
+        If :class:`str`, shell-style wildcards (e.g. ``*``) will be expanded.
 
     Returns
     -------
-    ds: xarray dataset
+    xarray.Dataset
+    """
+    from glob import glob
 
-    '''
-    
+    import xarray as xr
+
+    if isinstance(files, str):
+        filelist = sorted(glob(files, recursive=False))
+    elif isinstance(files, Path):
+        filelist = [files]
+    else:
+        filelist = files  # assume list
+
+    times = []
+    datasets = []
+    for filename in filelist:
+        data = _open_one_dataset(filename)
+        times.append(data.attrs.pop("time"))
+        datasets.append(data)
+
+    ds = xr.concat(datasets, dim="time")
+    ds["time"] = (("time"), times)
+    ds = ds.reset_coords().set_coords(["latitude", "longitude", "time"])
+
+    return ds
+
+
+def _open_one_dataset(fname):
+    """Read locally stored NASA Suomi NPP OMPS Level 3 Nadir Mapper TO3 file.
+
+    Parameters
+    ----------
+    fname: str
+        Local path to h5 file.
+
+    Returns
+    -------
+    xarray.Dataset
+    """
     import h5py
     import numpy as np
-    import xarray as xr
     import pandas as pd
-    from matplotlib import pyplot as plt
-    
-    with h5py.File(fname,'r') as f:
-        lat = f['Latitude'][:]
-        lon = f['Longitude'][:]
-        column = f['ColumnAmountO3'][:]
-        cloud_fraction = f['RadiativeCloudFraction'][:]
-        time = pd.to_datetime(f.attrs.__getitem__('Date').decode('UTF-8'),format='%Y-%m-%d')
+    import xarray as xr
 
-    # remove cloudy scenes and points with no data (eg. polar dark zone)
+    with h5py.File(fname, "r") as f:
+        lat = f["Latitude"][:]
+        lon = f["Longitude"][:]
+        column = f["ColumnAmountO3"][:]
+        cloud_fraction = f["RadiativeCloudFraction"][:]
+        time = pd.to_datetime(f.attrs.get("Date").decode("UTF-8"), format=r"%Y-%m-%d")
+
+    # Remove cloudy scenes and points with no data (eg. polar dark zone)
     column[(column < 0)] = np.nan
     column[(cloud_fraction > 0.3)] = np.nan
-    lon_2d,lat_2d = np.meshgrid(lon,lat)
-    
-    ds = xr.Dataset({'ozone_column':(['time','x','y'],column[None,:,:]),
-                    },
-                    coords={'longitude':(['x','y'],lon_2d),'latitude':(['x','y'],lat_2d),},
-                   attrs={'time':time})
-    
+    lon_2d, lat_2d = np.meshgrid(lon, lat)
+
+    ds = xr.Dataset(
+        {
+            "ozone_column": (["time", "x", "y"], column[None, :, :]),
+        },
+        coords={
+            "longitude": (["x", "y"], lon_2d),
+            "latitude": (["x", "y"], lat_2d),
+        },
+        attrs={"time": time},
+    )
+
     return ds
