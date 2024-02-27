5e560e021a553ec4128303c64326d8e555748d40
mebruckner <48494069+mebruckner@users.noreply.github.com>
2024-02-27 09:49:11 -0600

    remove reference to a reader not included
    Removed call to a reader that is only in personal branch

--- monetio/models/__init__.py
+++ monetio/models/__init__.py
@@ -13,7 +13,6 @@ from . import (
     pardump,
     prepchem,
     raqms,
-    fv3raqms,
 )
 
 __all__ = [
@@ -31,7 +30,6 @@ __all__ = [
     "prepchem",
     "pardump",
     "raqms",
-    "fv3raqms",
 ]
 
 __name__ = "models"
